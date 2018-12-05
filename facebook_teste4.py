4f9501048ea5b638e6468225f48a1c3869c327e3
carlosgabriel11 <cgaa1103@gmail.com>
2018-12-05 18:04:47 -0200

    Update facebook_teste4.py

--- facebook_teste4.py
+++ facebook_teste4.py
@@ -1,9 +1,16 @@
 import facebook
+import requests
+import urllib2
 #pegar o oken com o acesso a leads
-token = 'EAAQhV3JPZCU8BAGY4fynpKolnXsU7IhF3asMEZAZA2dIl01QepzjSpjlIftpi60FbtuoYIeNfTYSb5ITEnOEH60nf51mOPyftuZAqcu6H4hZA1cYy1JK3AZCR56ZCMP9rHkj33j5zj8JbrfkgSZABrsWWjjq32UejwNLiExihKUWDb0MHk2ErfCdb742XFKQbew2P8uALJJyuAZDZD'
+token = 'EAAQhV3JPZCU8BAA7xhnIHb0psJyrMhFy6LgveVxACsKapUS4bIiMNlHptrrN6YqnrBvIzuJS8S4KbfqGK5eGnuZCRx07iIxdnpSUpYF0muLNVC6eRWwIacVtSkTFl31TDovNDptA4o6M83eMHIaklfXBBfmEuS1FfAX4XeOlxU5h1iulcYk1PXdT0DGPwvvPFlK2hQpwZDZD'
 graph = facebook.GraphAPI(token)
-#no facebook business, pegar todos os fields que existem com o metadata=1 e substituir o nome pelo id do usuário business
-args = {'fields' : 'colocar o field da lead'}
+args = {'fields' : 'leadgen_forms'}
 #substituir talvez por get_object(s)
-friends = graph.get_connections("275176122991882", "")
-print friends
+friends = graph.get_object("me", **args)
+print friends['leadgen_forms']['data'][0]['leadgen_export_csv_url']
+
+response = urllib2.urlopen(friends['leadgen_forms']['data'][0]['leadgen_export_csv_url'])
+html = response.read()
+
+with open('.', 'w') as f:  
+    f.write(html) 
