41e6c0847f2f7b219b0a6965e924ce2e56f52467
carlosgabriel11 <cgaa1103@gmail.com>
2018-12-07 13:02:26 -0200

    Add files via upload

--- facebook_teste_data.py
+++ facebook_teste_data.py
@@ -2,45 +2,100 @@ import facebook
 import os
 import calendar
 import datetime
+import time
 from calendar import monthrange
 
 #referencia dos dias em que vai pegar os leads
-dias = input("Voce quer pegar os leads de quantos dias?\n")
+while True:
+	dias = input("Voce quer pegar os leads de quantos dias: ")
 
-#o token com acesso a leads
-token = 'EAAEfxROpy9IBAHzfP2SiEd5iEchf7huVrwbDGjbe5d4ORPg9ZBZAWS5OuiDdiYLxnGXmS6uV8J0Xkrh2E0itaFi8hefqQ6RxmZAG0ZC6AUgFtzGAqU5LBJl4wVphzXKnSFHMxWvU8mWA6EN7GQytP0Vak2MaUGrS7M1UMUFmViUZB462diqsPFM9kifXDq3cZD'
-graph = facebook.GraphAPI(token)
-args = {'fields' : 'leadgen_forms'}
-#pegando o objeto dicionario referente as leads
-friends = graph.get_object("me", **args)
-
-url = friends['leadgen_forms']['data'][0]['leadgen_export_csv_url']
+	if dias <= 0:
+		print "Dia invalido"
+		continue
+	else:
+		break
 
 now = datetime.datetime.now()
+
+#pegar o dia de origem que o usuario quer as leads
 if dias >= now.day:
 	var = dias - now.day
-	if now.month = 1:
+	if now.month == 1:
+		ano = now.year
 		mes = 12
 		dias_do_mes = monthrange(now.year-1, mes)
+		dia = dias_do_mes[1] - var
 	else:
+		ano = now.year
 		mes = now.month - 1
 	 	dias_do_mes = monthrange(now.year, mes)
-	 dia = dias_do_mes[1] - var
+	 	dia = dias_do_mes[1] - var
 else:
+	ano = now.year
 	mes = now.month
-	dia = now.day-dias
+	dia = now.day - dias
 
+#colocar no formato unix
+cal1 = calendar.timegm((ano, mes, dia, 0, 0, 0))
+cal2 = calendar.timegm((now.year, now.month, now.day, 0, 0, 0))
 
-cal1 = calendar.timegm((now.year, mes, dia, 0, 0, 0))
-cal2 = calendar.timegm((now.year, now.month, now.day-1, 24, 0, 0))
+#o token com acesso a leads
+token = []
+#token da Morar_BR
+token.append('EAAQhV3JPZCU8BAG0iWwcoKjeQug3wAT9ks4AiAvHGgMyOEj7yIMw67WTnliZB335zizemvBlktZAQRUZCJJZBiLdjI4rm3bgN9c6C985ZAdVPVJEK6ZAgaK93HHFROa7f10ZAXXIZAjPjRC32iHhvmXYX5wyhKay9jU3PmQnclfv4BwZDZD')
+#token da Cilios_BR
+token.append('EAAQhV3JPZCU8BAOQKUsn7BKZBjxVvrOb40y80pBvE2sOQqQiI3TETjsd01XyZB25ibFRazOhkeXl2IZBQKb6di9n1ZBiXDxHHMpSm3ctfLL4DGoILw4FGrF2FRcA3wZA65HdA5hjy66lHfXkZAOwg2F9BeaMZBOmqEd0DtmMgFJkb3oA2W7TtjGm')
 
-url = url[:(url.find("&")+1)] +  "type=form&amp&from_date=" + str(cal1) + "&amp&to_date=" + str(cal2)
+#os argumentos para pegar as leads
+args = {'fields' : 'leadgen_forms'}
 
-print url
+#o local de download dos arquivos
+diret = "C:\\Users\\renderxp\\Downloads\\"
 
-os.system("chrome \"" + url + "\"")
+#o contador global
+global_counter = 0
+
+while global_counter < len(token):
+
+	graph = facebook.GraphAPI(token[global_counter])
+
+	#pegando o objeto dicionario referente as leads
+	friends = graph.get_object("me", **args)
+
+	#baixar os arquivos
+	counter = 0
+
+	while counter < len(friends['leadgen_forms']['data']):
+		url = friends['leadgen_forms']['data'][counter]['leadgen_export_csv_url']
+
+		url = url[:(url.find("&")+1)] +  "type=form&amp&from_date=" + str(cal1) + "&amp&to_date=" + str(cal2)
+
+		os.system("chrome \"" + url + "\"")
+
+		counter = counter + 1
+
+	time.sleep(1)
+
+	#apagar as leads vazias
+	for nome in os.listdir(diret):
+		if nome.find(".csv") != -1:
+			new_diret = diret + nome
+
+			if os.path.getsize(new_diret) < 350:
+				os.remove(new_diret)
+
+	#renomear os arquivos para xls
+	for nome in os.listdir(diret):
+		if nome.find(".csv") != -1:
+			nome = diret + nome
+			new_name = nome[:nome.find(".csv")] + ".xls"
+			os.rename(nome, new_name)
+
+	global_counter = global_counter + 1
+
 #fp = csv.writer(open("algo.csv", "w"))
 
 #for line in response.content:
 #	print line
 #	fp.writerow(line)
+
