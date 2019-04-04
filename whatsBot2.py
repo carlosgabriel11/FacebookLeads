6f25457951cedccd56cb55174750fb61b7445cb7
carlosgabriel11 <cgaa1103@gmail.com>
2019-04-04 10:55:22 -0300

    Add files via upload

--- whatsBot2.py
+++ whatsBot2.py
@@ -59,31 +59,21 @@ url = 'https://web.whatsapp.com'
 url_phone  = 'https://api.whatsapp.com/send?phone='
 
 #the name of the workbook
-nome_planilha = 'Lista_Retiro001.xls'
+nome_planilha = 'Retiro_0002.xls'
 
-with open('texto.txt', 'r') as f:
-	conteudo = f.read()
-	clipboard.copy(',%0A\
-Estou%20enviando%20informações%20sobre%20o%20Retiro%20de%20Meditação%20de%2013%20e%2014%20de%20%20abril.%0A\
-Será%20no%20Paraíso%20na%20Terra,%20um%20local%20de%20natureza%20preservada,%20com%20muito%20ar%20puro,%20cachoeiras,%20canto%20de%20pássaros%20e%20quietude%20a%2040%20minutos%20do%20Plano%20Piloto%20(Brasília).%0A\
-No%20Retiro%20temos%20resultados%20incríveis%20de%20relaxamento,%20alegria%20e%20união%20de%20pessoas%20com%20o%20mesmo%20propósito:%20ter%20Harmonia%20e%20Equilíbrio%20em%20suas%20vidas.%0A\
-Serão%202%20dias%20dedicados%20a%20variadas%20práticas%20meditativas,%20com%20%20exercícios%20de%20silenciamento,%20serenização,%20contemplações,%20caminhadas%20e,%20para%20os%20que%20apreciam,%20banhos%20de%20cachoeira.%20Tudo%20isto,%20em%20meio%20ao%20verde%20da%20natureza,%20com%20uma%20alimentação%20super%20leve%20e%20natural.%0A\
-%0A\
-Informações%0A\
-Data:%20Sábado%20e%20Domingo,%20dias%2013%20e%2014/04/2019%0A\
-Período:%20de%207:30h%20de%20sábado%20(13/04)%20às%2016:00h%20de%20domingo%20(14/04).%0A\
-Local:%20Reserva%20Ecológica%20Paraíso%20na%20Terra%20em%20Brazlândia%20–%20DF,%20que%20fica%20a%2050%20min%20do%20Plano.%0A\
-Incluído%20no%20programa:%20Ensinamentos%20e%20vivências%20de%20Meditação%20Mindfulness.%20Hospedagem%20e%20alimentação%20completa%20(café%20da%20manhã,%20almoço,%20jantar%20e%20coffee-break%20no%20sábado%20e%20café%20da%20manhã,%20coffee-break%20e%20almoço%20no%20domingo).%0A\
-Valores%0A\
-Qto%20triplo:%20R$%20570%20à%20vista%20ou%20em%202%20x%20de%20R$%20315%0A\
-Qto%20duplo:%20R$%20590%20à%20vista%20ou%20em%202%20x%20de%20R$%20330%0A\
-Qto%20casal%20(por%20pessoa):%20R$%20590%20à%20vista%20ou%20em%202%20x%20de%20R$%20330%0A\
-Qto%20individual:%20R$%20690%20à%20vista%20ou%20em%202%20x%20de%20R$%20390%0A\
-%0A\
-Se%20você%20desejar%20viver%20essa%20experiência,%20é%20só%20responder%20essa%20mensagem%20que%20entraremos%20em%20contato%20por%20telefone.%0A\
-%0A\
-André')
-
+mensagem = ',%0A\
+Está%20chegando%20a%20hora%20do%20Retiro%20de%20Meditação.%0A\
+Quem%20quer%20serenizar%20a%20mente%20e%20energizar%20o%20corpo%20já%20se%20inscreveu%0A\
+Será%20em%20um%20local%20mágico,%20a%2050%20min%20do%20Plano%20Piloto.%20Veja%20um%20álbum%20com%20fotos%20e%20vídeos%20de%20Retiros%20que%20já%20fizemos:%20https://photos.app.goo.gl/C5kM3ca3FPwgJ39S9%0A\
+O%20Retiro%20será%20coordenado%20pela%20Profa%20Cristina%20Gobbi,%20estudiosa%20e%20praticante%20de%20Meditação%20desde%201995.%0A\
+Quer%20saber%20mais%20sobre%20ela?%20Acesse%20o%20canal%20no%20Youtube%0A\
+http://www.youtube.com/c/MeditaçãoHarmoniaeEquilíbrio%0A\
+Ou%20conheça%20sua%20Página%20no%20Face%0A\
+http://www.facebook.com/meditacaoharmoniaequilibrio/%0A\
+Temos%20também%20o%20Perfil%20no%20Insta%0A\
+http://www.instagram.com/meditacao_harmonia_equilibrio/%0AE%20o%20Site:%20www.cristinagobbi.com.br%0A\
+Temos%20mais%20interessados%20do%20que%20acomodações.%0A\
+Só%20falta%20você.%20Vamos?'
 #opening the workbook
 book = xlrd.open_workbook(nome_planilha)
 
@@ -111,7 +101,7 @@ wbInexistente = xlwt.Workbook()
 
 wsInexistente = wbInexistente.add_sheet("Inexistente")
 
-for counter in range(sh.nrows):
+for counter in range(100):
 	if counter == 0:
 		continue
 
@@ -132,9 +122,11 @@ for counter in range(sh.nrows):
 
 		new_url = url_phone + phone + "&text="
 
-		new_message = name + clipboard.paste()
+		new_message = name + mensagem
 
 		new_url = new_url + new_message
+
+		print new_url
 
 		browser.get(new_url)
 
@@ -144,7 +136,7 @@ for counter in range(sh.nrows):
 
 		browser.find_element_by_class_name("button").click()
 
-		sleep(10)
+		sleep(26)
 
 		text_box = browser.find_element_by_class_name("_2S1VP")
 		text_box.send_keys('\n')		
